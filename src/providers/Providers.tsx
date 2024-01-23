4592b4d3e498d29532c6215c7aff32316dae1fe1
Lalit sharma <geekymonkcp742k@gmail.com>
2024-01-23 14:02:39 +0530

    feat(Providers): Implement custom provider integrating Next Themes and NextAuth for theme switching and authentication.

---
+++ src/providers/Providers.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import * as React from 'react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { type ThemeProviderProps } from 'next-themes/dist/types';
+import { SessionProvider } from 'next-auth/react';
+
+// const queryClient = new QueryClient();
+export function Provider({ children, ...props }: ThemeProviderProps) {
+  return (
+    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem {...props}>
+      <SessionProvider>{children}</SessionProvider>
+    </NextThemesProvider>
+  );
+}
