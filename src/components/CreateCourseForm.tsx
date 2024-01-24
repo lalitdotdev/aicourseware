59768b41a866de0725c46915142b113c01da8e6f
Lalit sharma <geekymonkcp742k@gmail.com>
2024-01-25 01:28:59 +0530

    feat(createcourse react-hook-form with schema validation): Implement CreateCourseForm component with form structure, validation using zod, and dynamic addition/removal of units. Utilize react-hook-form, zod, and framer-motion for a seamless form experience.

---
+++ src/components/CreateCourseForm.tsx
@@ -0,0 +1,107 @@
+'use client';
+
+import { createChaptersSchema } from '@/lib/validators/course';
+import { FC } from 'react';
+import { z } from 'zod';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from './ui/form';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Input } from './ui/input';
+import { Separator } from './ui/separator';
+import { Button } from './ui/button';
+import { Plus, Trash } from 'lucide-react';
+import { motion, AnimatePresence } from 'framer-motion';
+
+interface CreateCourseFormProps {}
+
+// creating type from zod object
+type formSchema = z.infer<typeof createChaptersSchema>;
+
+const CreateCourseForm: FC<CreateCourseFormProps> = ({}) => {
+  const form = useForm<formSchema>({
+    resolver: zodResolver(createChaptersSchema),
+    defaultValues: {
+      title: '',
+      units: ['', '', ''],
+    },
+  });
+
+  function onSubmit(formData: formSchema) {
+    console.log(formData);
+  }
+
+  //   console.log(form.watch());
+  return (
+    <div className="w-full">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-full mt-4">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem className="flex flex-col items-start w-full sm:items-center sm:flex-row">
+                <FormLabel className="flex-[1] text-xl">Title</FormLabel>
+                <FormControl className="flex-[6]">
+                  <Input placeholder="Enter the main topic of the course " {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {form.watch('units').map((_, index) => (
+            <FormField
+              control={form.control}
+              name={`units.${index}`}
+              key={index}
+              render={({ field }) => (
+                <FormItem className="flex flex-col items-start w-full sm:items-center sm:flex-row">
+                  <FormLabel className="flex-[1] text-xl">Unit {index + 1}</FormLabel>
+                  <FormControl className="flex-[6]">
+                    <Input placeholder="Enter subtopic of the course " {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          ))}
+
+          <div className="flex items-center justify-center mt-4">
+            <Separator className="flex-[1]" />
+            <div className="mx-4">
+              <Button
+                variant="secondary"
+                type="button"
+                className="font-semibold ml-2 gap-1 "
+                onClick={() => {
+                  form.setValue('units', [...form.watch('units'), '']);
+                }}
+              >
+                <Plus className="w-4 h-4 ml-2 text-green-500" />
+                Add Unit
+              </Button>
+
+              <Button
+                variant="secondary"
+                type="button"
+                className="font-semibold ml-2 gap-1"
+                onClick={() => {
+                  form.setValue('units', [...form.watch('units').slice(0, -1)]);
+                }}
+              >
+                <Trash className="w-4 h-4 ml-2 text-green-500" />
+                Remove Unit
+              </Button>
+            </div>
+            <Separator className="flex-[1]" />
+          </div>
+          <Button type="submit" className="w-full mt-6" size="lg">
+            Let&apos;s Go!
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default CreateCourseForm;
