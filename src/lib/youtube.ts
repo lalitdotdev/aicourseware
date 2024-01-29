63771a4384703f3a58c48945cc553d614cad904d
Lalit sharma <geekymonkcp742k@gmail.com>
2024-01-29 17:43:40 +0530

    feat(getTranscript youtube video): Add getTranscript function to retrieve transcript from a YouTube video ID using the youtube-transcript library. The function fetches the transcript in English language from the specified video ID and formats it for use.

--- src/lib/youtube.ts
+++ src/lib/youtube.ts
@@ -19,4 +19,20 @@ export async function searchYoutube(searchQuery: string) {
   return videoId;
 }
 
-// TODO: get transcript from youtube video id
+// get transcript from youtube video Id
+export async function getTranscript(videoId: string) {
+  try {
+    let transcript_arr = await YoutubeTranscript.fetchTranscript(videoId, {
+      lang: 'en',
+      country: 'EN',
+    });
+    let transcript = '';
+    for (let t of transcript_arr) {
+      transcript += t.text + ' ';
+    }
+    return transcript.replaceAll('\n', ' ');
+  } catch (error) {
+    console.log(error);
+    return '';
+  }
+}
