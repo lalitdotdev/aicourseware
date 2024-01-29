8ae006dbd37fa9a56274bbec0384b7fe53125855
Lalit sharma <geekymonkcp742k@gmail.com>
2024-01-29 17:46:32 +0530

    feat(api/chapter/getInfo route): Create new API route to handle fetching chapter information asynchronously. The route parses the request body, retrieves chapter details from the database, searches for a corresponding YouTube video, retrieves its transcript, and generates a summary using GPT-3. Error handling is implemented to handle validation errors and unexpected failures.

--- src/app/api/chapter/getInfo/route.ts
+++ src/app/api/chapter/getInfo/route.ts
@@ -1,20 +1,90 @@
+import { db } from '@/lib/db';
+import { strict_output } from '@/lib/gpt';
+import { getTranscript, searchYoutube } from '@/lib/youtube';
 import { NextResponse } from 'next/server';
 import { resolve } from 'path';
+import { z } from 'zod';
 
 // api/chapter/getInfo
 
 // sleep for 0-4 seconds to simulate async behavior of real world api calls to database etc.
 
-const sleep = async () =>
-  new Promise((resolve) => {
-    setTimeout(resolve, Math.random() * 4000);
-  });
+const bodyParser = z.object({
+  // converting the body to a zod schema to validate it and get the types
+  chapterId: z.string(),
+});
+// const sleep = async () =>
+//   new Promise((resolve) => {
+//     setTimeout(resolve, Math.random() * 4000);
+//   });
 
 export async function POST(req: Request, res: Response) {
   try {
-    await sleep();
-    return NextResponse.json({
-      message: 'Hello World',
+    const body = await req.json();
+    const { chapterId } = bodyParser.parse(body);
+
+    // Get Chapter Info from Database using chapterId
+
+    const chapter = await db.chapter.findUnique({
+      where: {
+        id: chapterId,
+      },
     });
-  } catch (error) {}
+
+    if (!chapter) {
+      return NextResponse.json(
+        {
+          success: false,
+          error: 'Chapter not found',
+        },
+        {
+          status: 404,
+        },
+      );
+    }
+
+    // Get Youtube Video Id from Youtube Search Query
+    const videoId = await searchYoutube(chapter.youtubeSearchQuery);
+
+    // Get Transcript from Youtube Video Id
+    let videoTranscript = await getTranscript(videoId);
+
+    // Getting summary from transcript using GPT-3 API (OpenAI) and returning it in the response (strict_output is a helper function to make the API call to GPT-3) (see lib/gpt.ts)
+    let maxLength = 500;
+    videoTranscript = videoTranscript.split(' ').slice(0, maxLength).join(' ');
+    const { summary }: { summary: string } = await strict_output(
+      'You are an AI capable of summarising a youtube transcript',
+      'summarise in 250 words or less and do not talk of the sponsors or anything unrelated to the main topic, also do not introduce what the summary is about.\n' +
+        videoTranscript,
+      { summary: 'summary of the transcript' },
+    );
+
+    return NextResponse.json({
+      videoId,
+      videoTranscript,
+      summary,
+    });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return NextResponse.json(
+        {
+          success: false,
+          error: error.issues,
+        },
+        {
+          status: 400,
+        },
+      );
+    } else {
+      return NextResponse.json(
+        {
+          success: false,
+          error: 'Something went wrong',
+        },
+        {
+          status: 500,
+        },
+      );
+    }
+  }
 }
