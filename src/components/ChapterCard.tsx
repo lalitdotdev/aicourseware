e820369e2fc8aaaac66308f6f0b9e557d8917c9d
Lalit sharma <geekymonkcp742k@gmail.com>
2024-01-30 14:47:36 +0530

    refactor(state of loading): chaptercard and confirmChapters

--- src/components/ChapterCard.tsx
+++ src/components/ChapterCard.tsx
@@ -11,54 +11,60 @@ import { forwardRef, useCallback, useEffect, useImperativeHandle, useState } fro
 type ChapterCardProps = {
   chapter: Chapter;
   chapterIndex: number;
-  completedChapters: Set<String>;
-  setCompletedChapters: React.Dispatch<React.SetStateAction<Set<String>>>;
+  //   completedChapters: Set<String>;
+  //   setCompletedChapters: React.Dispatch<React.SetStateAction<Set<String>>>;
 };
 
 export type ChapterCardHandler = {
   triggerLoad: () => void;
 };
 
-const ChapterCard = forwardRef<ChapterCardHandler, ChapterCardProps>(
-  ({ chapter, chapterIndex, setCompletedChapters, completedChapters }, ref) => {
-    const { toast } = useToast();
-    const [success, setSuccess] = useState<boolean | null>(null);
-    const { mutate: getChapterInfo, isPending } = useMutation({
-      mutationFn: async () => {
-        const response = await axios.post('/api/chapter/getInfo', {
-          chapterId: chapter.id,
-        });
-        return response.data;
-      },
-    });
-    useImperativeHandle(ref, () => ({
-      triggerLoad: () => {
-        // console.log('trigger load');
-        // TODO: trigger load here and set success to true or false depening on the result of the load function call (which will be an api call) and then set the success state to true or false
-        getChapterInfo(undefined, {
-          onSuccess: () => {
-            console.log('success');
-            // setSuccess(true);
-          },
-        });
-      },
-    }));
+const ChapterCard = forwardRef<ChapterCardHandler, ChapterCardProps>(({ chapter, chapterIndex }, ref) => {
+  const { toast } = useToast();
+  const [success, setSuccess] = useState<boolean | null>(null);
+  const { mutate: getChapterInfo, isPending } = useMutation({
+    mutationFn: async () => {
+      const response = await axios.post('/api/chapter/getInfo', {
+        chapterId: chapter.id,
+      });
+      return response.data;
+    },
+  });
+  useImperativeHandle(ref, () => ({
+    triggerLoad: () => {
+      // console.log('trigger load');
+      // TODO: trigger load here and set success to true or false depening on the result of the load function call (which will be an api call) and then set the success state to true or false
+      getChapterInfo(undefined, {
+        onSuccess: () => {
+          setSuccess(true);
+        },
+        onError: (err) => {
+          console.error(err);
+          setSuccess(false);
+          toast({
+            variant: 'destructive',
+            title: 'Error',
+            description: 'An error occured while loading the chapter',
+          });
+        },
+      });
+    },
+  }));
 
-    return (
-      <div
-        key={chapter.id}
-        className={cn('px-4 py-2 mt-2 rounded flex justify-between', {
-          'bg-secondary': success === null,
-          'bg-red-500': success === false,
-          'bg-green-500': success === true,
-        })}
-      >
-        <h5>{chapter.name}</h5>
-        {isPending && <Loader2 className="animate-spin" />}
-      </div>
-    );
-  },
-);
+  return (
+    <div
+      key={chapter.id}
+      className={cn('px-4 py-2 mt-2 rounded flex justify-between', {
+        'bg-secondary': success === null,
+        'bg-red-500': success === false,
+        'bg-green-500': success === true,
+      })}
+    >
+      <h5>{chapter.name}</h5>
+      {isPending && <Loader2 className="animate-spin" />}
+    </div>
+  );
+});
 
 ChapterCard.displayName = 'ChapterCard';
 
