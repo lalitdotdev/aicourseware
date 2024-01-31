7df447b1ec3cfe429c003084373463c6dcef64e6
Lalit sharma <geekymonkcp742k@gmail.com>
2024-02-01 02:00:38 +0530

    feat(ui): Refactor QuizCards component structure and implement dynamic question state management.

--- src/components/QuizCards.tsx
+++ src/components/QuizCards.tsx
@@ -5,6 +5,8 @@ import { Label } from './ui/label';
 import { Button } from './ui/button';
 import { ChevronRight } from 'lucide-react';
 import { RadioGroup, RadioGroupItem } from './ui/radio-group';
+import { cn } from '@/lib/utils';
+import { useState } from 'react';
 
 type Props = {
   chapter: Chapter & {
@@ -13,14 +15,23 @@ type Props = {
 };
 
 const QuizCards = ({ chapter }: Props) => {
+  const [questionState, setQuestionState] = useState<Record<string, boolean | null>>({});
+
   return (
     <div className="flex-[1] mt-16 ml-8">
       <h1 className="text-2xl font-bold">Concept Check</h1>
       <div className="mt-2">
         {chapter.questions.map((question) => {
-          const options = JSON.parse(question.options) as string[];
+          const options = JSON.parse(question.options) as string[]; // this is the array of options for the question that we need to parse from the json string in the database
           return (
-            <div key={question.id}>
+            <div
+              key={question.id}
+              className={cn('p-3 mt-4 border border-secondary rounded-lg', {
+                'bg-green-700': questionState[question.id] === true,
+                'bg-red-700': questionState[question.id] === false,
+                'bg-secondary': questionState[question.id] === null,
+              })}
+            >
               <h1 className="text-lg font-semibold">{question.question}</h1>
               <div className="mt-2">
                 <RadioGroup onValueChange={(e) => {}}>
